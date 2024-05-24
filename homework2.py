c6bb486932c9e3c866f3677ae7b00a55ed1a5535
rinat-proff <rinat-proff@mail.ru>
2024-05-24 20:47:19 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+first_name = "Всего задач:"
+a = 12
+second_name = "затрачено часов:"
+b = 1.5
+name = "Курс: Python,"
+print(b / a)
+c = 0.125
+third_name = "среднее время выполнения"
+fourth_name = "часа."
+print(name, first_name, a,',', second_name, b,',', third_name, c, fourth_name)
+
+
